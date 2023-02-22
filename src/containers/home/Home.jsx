f171e0163b0e6122ba11decde8f19011066fad24
Dale Cottrell <dalecottrell@Dales-MacBook-Pro.local>
2023-02-22 10:49:54 -0800

    add back landing

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { getAllProducts } from '../../redux/reducers/products'
+// import Auth from '../../components/auth/Home'
+import './home.css'
 
 import ProductsList from '../../components/products-list/ProductsList'
 
@@ -10,6 +12,9 @@ const Home = props => {
     
     return (
         <div className="home">
+        
+        <img src="https://i.ibb.co/Pjnfs0G/69-landing.jpg" alt="landing" className="home-img"/>
+
             <ProductsList products={products}/>
         </div>
     )
