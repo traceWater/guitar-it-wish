6c2e6edcc849f8af655da3da34be6c3bfb1c35ac
Dale Cottrell <dalecottrell@Dales-MacBook-Pro.local>
2023-02-22 10:42:38 -0800

    add auth

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,39 +1,39 @@
-import React, {Component} from 'react';
-import {Field, reduxForm} from 'redux-form';
-import * as actions from '../../actions';
-import {connect} from 'react-redux';
+// import React, {Component} from 'react';
+// import {Field, reduxForm} from 'redux-form';
+// import * as actions from '../../actions';
+// import {connect} from 'react-redux';
 
-import {renderAlert} from '../../utils/auxiliary';
-import {renderFormField} from '../../utils/form';
+// import {renderAlert} from '../../utils/auxiliary';
+// import {renderFormField} from '../../utils/form';
 
-class Signin extends Component {
+// class Signin extends Component {
 
-  handleFormSubmit(formProps) {
-   this.props.signinUser(formProps);
-  }
+//   handleFormSubmit(formProps) {
+//    this.props.signinUser(formProps);
+//   }
 
-  render() {
+//   render() {
 
-    const {handleSubmit} = this.props;
+//     const {handleSubmit} = this.props;
 
-    return (
-        <form onSubmit = {handleSubmit(this.handleFormSubmit.bind(this))}>
-          <Field name="email" type="text" label="Email" component={renderFormField} />
-          <Field name="password" type="password" label="Password" component={renderFormField} />
-          <button type="submit" className="btn btn-success">Sign in</button>
-          {renderAlert(this.props.errorInMessage)}
-        </form>
-    );
-  }
-}
+//     return (
+//         <form onSubmit = {handleSubmit(this.handleFormSubmit.bind(this))}>
+//           <Field name="email" type="text" label="Email" component={renderFormField} />
+//           <Field name="password" type="password" label="Password" component={renderFormField} />
+//           <button type="submit" className="btn btn-success">Sign in</button>
+//           {renderAlert(this.props.errorInMessage)}
+//         </form>
+//     );
+//   }
+// }
 
-function mapStateToProps(state) {
-  return {errorInMessage: state.auth.errorin};
-}
+// function mapStateToProps(state) {
+//   return {errorInMessage: state.auth.errorin};
+// }
 
-export default reduxForm({
-  form: 'signin'
-})
-(connect(mapStateToProps, actions)(Signin));
+// export default reduxForm({
+//   form: 'signin'
+// })
+// (connect(mapStateToProps, actions)(Signin));
 
 
